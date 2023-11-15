1fb70904cef06bb646c10bae3f4ee05d656513d9
hamzarzine2 <hamza.rzine@student.vinci.be>
2023-11-15 14:06:13 +0100

    add : auth + eureka microService

---
+++ auth/src/main/java/com/example/auth/AuthenticationRepository.java
@@ -0,0 +1,10 @@
+package com.example.auth;
+
+
+import com.example.authentification.models.SafeCredentials;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthenticationRepository extends CrudRepository<SafeCredentials, String> {
+}
