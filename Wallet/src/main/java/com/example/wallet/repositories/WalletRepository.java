3a8fc2ec4f73e9251be2daeae3b270409ea8a373
Andrei Avram <andrei.Avram@student.vinci.Be>
2023-11-29 23:18:34 +0100

    Tests

--- Wallet/src/main/java/com/example/wallet/repositories/WalletRepository.java
+++ Wallet/src/main/java/com/example/wallet/repositories/WalletRepository.java
@@ -2,16 +2,13 @@ package com.example.wallet.repositories;
 
 import com.example.wallet.models.Position;
 import java.util.List;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WalletRepository extends CrudRepository<Position, String>{
 
-  @Query("select p from positions p where p.username =: username and p.ticker =: ticker ")
-  Position getUserPosition(@Param("username") String username, @Param("ticker") String ticker);
+  Position getPositionsByUsernameAndTicker(String username, String ticker);
 
   List<Position> getAllByUsernameEquals(String username);
 }
