17afe81a27cd9cf59ed5af1f29ffb53e3697d7f2
hamzarzine2 <hamza.rzine@student.vinci.be>
2023-11-30 18:07:41 +0100

    adding javadoc for service

--- Execution/src/main/java/com/example/execution/ExecutionService.java
+++ Execution/src/main/java/com/example/execution/ExecutionService.java
@@ -13,6 +13,13 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This service class handles the execution of orders, updates positions in the wallet, and communicates
+ * with various proxies to handle transactions.
+ *
+ * @author hamza
+ * @version 1.0
+ */
 @Service
 public class ExecutionService {
 
@@ -25,6 +32,15 @@ public class ExecutionService {
     @Autowired
     private OrderProxy orderProxy;
 
+    /**
+     * Executes a transaction, updating the buyer, seller, and related entities.
+     *
+     * @param ticker      the ticker symbol of the transaction
+     * @param seller      the seller's username
+     * @param buyer       the buyer's username
+     * @param transaction the transaction details
+     * @return {@code true} if the execution is successful, {@code false} otherwise
+     */
     public boolean executeOrder(String ticker, String seller, String buyer, Transaction transaction) {
         try {
             return  executeupdateBuyer(transaction,buyer) && executeupdateSeller(transaction,seller) &&
@@ -37,6 +53,13 @@ public class ExecutionService {
 
     }
 
+    /**
+     * Executes updates for the seller's position and cash in the wallet microservice.
+     *
+     * @param transaction the transaction details
+     * @param user        the seller's username
+     * @return {@code true} if the execution is successful, {@code false} otherwise
+     */
     private boolean executeupdateSeller(Transaction transaction, String user){
         List<Position> positionList = new ArrayList<>();
         Position positionTicket = new Position();
@@ -51,6 +74,14 @@ public class ExecutionService {
         System.out.println(walletResponse.getStatusCode());
         return true;
     }
+
+    /**
+     * Executes updates for the buyer's position and cash in the wallet microservice.
+     *
+     * @param transaction the transaction details
+     * @param user        the buyer's username
+     * @return {@code true} if the execution is successful, {@code false} otherwise
+     */
     private boolean executeupdateBuyer(Transaction transaction, String user ){
         List<Position> positionList = new ArrayList<>();
         Position positionTicket = new Position();
@@ -65,6 +96,12 @@ public class ExecutionService {
         return true;
     }
 
+    /**
+     * Creates a position representing cash.
+     *
+     * @param amount the amount of cash
+     * @return a {@code Position} object representing cash
+     */
     private Position createPositionCash(double amount){
         Position positionCash = new Position();
         positionCash.setTicker("CASH");
@@ -73,11 +110,24 @@ public class ExecutionService {
         return positionCash;
     }
 
+    /**
+     * Handles the response from a proxy, printing the status code and returning the success status.
+     *
+     * @param response the response entity from the proxy
+     * @return {@code true} if the response indicates success, {@code false} otherwise
+     */
     private boolean handleResponse(ResponseEntity<Void> response) {
         System.out.println(!response.getStatusCode().isError());
         return !response.getStatusCode().isError();
     }
 
+    /**
+     * Updates the specified order with the given quantity by calling the order microservice.
+     *
+     * @param orderId  the ID of the order to be updated
+     * @param quantity the quantity to update the order with
+     * @return {@code true} if the update is successful, {@code false} otherwise
+     */
     private boolean updateOrder(String orderId, int quantity) {
         Order orderTemp = new Order();
         orderTemp.setFilled(quantity);
