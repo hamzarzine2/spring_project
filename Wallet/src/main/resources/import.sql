57b37a807dfe55875684790ba8f8bb7ac49d6f84
Andrei Avram <andrei.Avram@student.vinci.Be>
2023-11-30 13:31:41 +0100

    changed the import

--- Wallet/src/main/resources/import.sql
+++ Wallet/src/main/resources/import.sql
@@ -1,6 +1,6 @@
 insert into positions (username, ticker, quantity, unit_value) values ('Jacques', 'AAPL', 10, 100);
 insert into positions (username, ticker, quantity, unit_value) values ('Jacques', 'ADPL', 100, 200);
-insert into positions (username, ticker, quantity, unit_value) values ('Jacques', 'EDPL', 1000, 200);
+insert into positions (username, ticker, quantity, unit_value) values ('Jacques', 'EDPL', -1000, 200);
 insert into positions (username, ticker, quantity, unit_value) values ('Jacques', 'GJEL', 10, 20);
 insert into positions (username, ticker, quantity, unit_value) values ('Jacques', 'OPLS', 100, 20);
 insert into positions (username, ticker, quantity, unit_value) values ('Jacques', 'PLZ', 10, 20);
